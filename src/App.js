f4f211fe3460cfecef96e61c0b6580045db1c7bd
Charlyln <49599325+Charlyln@users.noreply.github.com>
2023-01-19 22:39:06 +0100

    Update iframe

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ function App() {
           <iframe
             id='iframe'
             title='newhope'
-            src='https://play.gx.games/game-wrapper/d2ff5fe5-4a49-40d1-aca1-7b497a2dbc83/index.html?game=d9710581-ad8d-4298-8e29-0b0166ccad20&track=d181ad25-4334-4e4b-bbc2-87418110461e&release=28f345ca-0a37-40da-9343-4d9b1ed8cb07&userId=9547171b-f050-4756-a417-777c8f86642d&username=charlypse&avatarUrl&gamePlayId=fe2f2977-ec38-487e-8e32-4dd2a194e0ac&coverImage=https://play.gxc.gg/game/d9710581-ad8d-4298-8e29-0b0166ccad20/cover/2a136eee-34f3-4f93-8046-2b4309ea1465?79a981994dcf746d26837c99f677026c'
-            style={{
+src='https://play.gx.games/game-wrapper/d2ff5fe5-4a49-40d1-aca1-7b497a2dbc83/index.html?game=d9710581-ad8d-4298-8e29-0b0166ccad20&track=d181ad25-4334-4e4b-bbc2-87418110461e&release=6f066f88-9777-4013-ab1d-46ca412baea0&userId=9547171b-f050-4756-a417-777c8f86642d&username=charlypse&avatarUrl&gamePlayId=58a59b06-e0c9-4bc1-a175-6b83438af38f&coverImage=https://play.gxc.gg/game/d9710581-ad8d-4298-8e29-0b0166ccad20/cover/2a136eee-34f3-4f93-8046-2b4309ea1465?79a981994dcf746d26837c99f677026c'
+           style={{
               border: 'none',
               width: '100%',
               height: '100%',
